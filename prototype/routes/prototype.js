27a8aa5acbc2e72d6fdb2f5873ff7c280d78aef7
Daniel Kim <dhkrock@outlook.com>
2021-12-09 10:23:57 -0500

    initialized OAuth 2.0, no db/html yet

--- prototype/routes/prototype.js
+++ prototype/routes/prototype.js
@@ -1,7 +1,10 @@
-var fetch = require('node-fetch');
-var express = require('express');
-var router = express.Router();
-var request = require('request');
+const fetch = require('node-fetch');
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+const passport = require('passport')
+const cookieSession = require('cookie-session')
+require('./passport-setup')
 
 router.get('/', function(req, res, next) {
     res.render('index');
@@ -19,14 +22,37 @@ router.post('/search', function(req, res, next) {
     })
 });
 
+router.use(cookieSession({
+    name: 'session',
+    keys: ['key1','key2']
+}))
 
-/* router.post('/search', function(req, res, next) {
-    request.get(`https://api.opentripmap.com/0.1/en/places/geoname?apikey=5ae2e3f221c38a28845f05b6170581c1ae28d0679637cd84a6811dbb`, function callback(err, httpResponse, body){
-        res.render('index', {search_result: body}); //${req.body.search}
-    })
-}); */
+const loginCheck = (req, res, next) => {
+    if (req.user) {
+        next()
+    } else {
+        res.sendStatus(401);
+    }m
+}
 
+router.use(passport.initialize())
+router.use(passport.session())
 
+router.get('/failed', (req,res) => res.send('Login failed'))
+router.get('/success',  loginCheck, (req,res) => res.send(`Welcome ${req.user.displayName}!`))
+
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }),
+    function(req, res) {
+        res.redirect('/success');
+    });
+
+router.get('/logout', (req,res) => {
+    req.session = null
+    req.logout()
+    res.redirect('/')
+})
 
 router.get
 module.exports = router;
